db860c692412f28089f3aee46218b32a91cf77c4
HUSTHY <840499869@qq.com>
2019-05-04 23:54:54 +0800

    SKL库LogisticRegression函数调用分类

--- Logistic/LogisticPraticalApplicationForHorseDeathPrediction.py
+++ Logistic/LogisticPraticalApplicationForHorseDeathPrediction.py
@@ -1,7 +1,21 @@
 import numpy as np
 import random
 import time
+from sklearn.linear_model import LogisticRegression as LR
 def dataPrePrcossing(dataFileName):
+    """
+        Description:数据文件中获取数据
+        Params:
+                dataFileName——数据文件名称(包含路劲)
+        Return:
+                trainDataSet——训练集
+                trainLabel——分类标签
+                pureHorseTotalTrainDataSet——训练集合分类标签集
+        Author:
+                HY
+        Modify:
+                2019/5/4 18:15
+    """
     trainDataSet=[];trainLabel=[];pureHorseTotalTrainDataSet=[]
     with open(dataFileName,'r') as file:
         content=file.readlines()
@@ -93,24 +107,25 @@ def gradAscent(dataMat,labels):
     bestWeights=np.ones(n)
     aplha=0.001
     biggestAccurate=0
-    maxCycle=0
-    for i in range(1,500,50):
-        weights=np.ones((n,1))
-        cycle=0
-        while cycle<i:
+    # maxCycle=0
+    # for i in range(1,40002,10000):
+    weights=np.ones((n,1))
+    cycle=0
+    while cycle<30001:
             #使用梯度上升矢量公式
-            r=sigmoid(dataMatrix*weights)
-            h=labelMat-r
-            temp=aplha*dataMatrix.transpose()*h
-            weights=weights+temp
-            cycle+=1
-        accurte=calculateAccurate(dataMatrix,labels,weights)
-        if biggestAccurate<accurte:
-            biggestAccurate=accurte
-            maxCycle=i
-            bestWeights=weights
-    print('梯度上升法：训练集中————最佳迭代次数:%d——分类准确率%d%%'%(maxCycle,biggestAccurate*100))
-    return bestWeights
+        r=sigmoid(dataMatrix*weights)
+        h=labelMat-r
+        temp=aplha*dataMatrix.transpose()*h
+        weights=weights+temp
+        cycle+=1
+    accurte=calculateAccurate(dataMatrix,labels,weights)
+        # if biggestAccurate<accurte:
+        #     biggestAccurate=accurte
+        #     maxCycle=i
+        #     bestWeights=weights
+    # print('梯度上升法：训练集中————最佳迭代次数:%d——分类准确率%d%%'%(maxCycle,biggestAccurate*100))
+    print('梯度上升法：训练集中分类准确率%d%%' % (accurte*100))
+    return weights
 
 
 def gradScochasticAscent(dataMat,labels):
@@ -131,10 +146,10 @@ def gradScochasticAscent(dataMat,labels):
     dataMatrix=np.array(dataMat)
     m,n=np.shape(dataMatrix)
     bestWeights=np.ones(n)
-    biggestAccurate=0
-    for j in range(1000):
+    weights = np.ones(n)
+    # biggestAccurate=0
+    for j in range(435):
         dataIndex=list(range(m))
-        weights=np.ones(n)
         for i in range(m):
             randomIndex=int(random.uniform(0,len(dataIndex)))#选择随机的一个样本来更新回归系数
             aplha=1/(1+i+j)+0.001
@@ -142,25 +157,55 @@ def gradScochasticAscent(dataMat,labels):
             temp=labels[randomIndex]-h
             weights=weights+aplha*dataMatrix[randomIndex].transpose()*temp
             del(dataIndex[randomIndex])
-        accurte=calculateAccurate(dataMatrix,labels,weights)
-        if biggestAccurate<accurte:
-            biggestAccurate=accurte
-            maxCycle=j
-            bestWeights=weights
-    print('随机梯度上升法中：最佳迭代次数:%d——分类准确率%d%%'%(maxCycle,biggestAccurate*100))
-    return bestWeights
+    accurte=calculateAccurate(dataMatrix,labels,weights)
+    print('随机梯度上升法中：分类准确率%d%%' % (accurte*100))
+    return weights
 
 def myOwnLogisitcClassificationResult(trainDataSet,trainLabel,testDataSet,testLabel,functionType):
+    """
+        Description:使用自己写的分类器进行分类的预测
+        Params:
+                trainDataSet——训练集数据
+                trainLabel——训练集分类标签集
+                testDataSet——测试集数据
+                testLabel——测试集分类标签集
+                functionType——0：批量梯度；1：随机梯度
+        Return:
+
+        Author:
+                HY
+        Modify:
+                2019/5/4 18:57
+    """
     if functionType==0:
         weights=gradAscent(trainDataSet,trainLabel)
-        # result=calculateAccurate(testDataSet,testLabel,weights)
-        result = calculateAccurate(trainDataSet, trainLabel, weights)
-        print('梯度上升法分类结果的准确率：%d %%'%(result*100))
+        result=calculateAccurate(testDataSet,testLabel,weights)
+        # result = calculateAccurate(trainDataSet, trainLabel, weights)
+        print('梯度上升法分类结果的准确率：%f %%'%(result*100))
     else:
         weights=gradScochasticAscent(trainDataSet,trainLabel)
-        # result = calculateAccurate(testDataSet, testLabel, weights)
-        result = calculateAccurate(trainDataSet, trainLabel, weights)
-        print('随机梯度上升法分类结果的准确率：%d %%' % (result * 100))
+        result = calculateAccurate(testDataSet, testLabel, weights)
+        # result = calculateAccurate(trainDataSet, trainLabel, weights)
+        print('随机梯度上升法分类结果的准确率：%f %%' % (result * 100))
+
+def SKLLogisticRegressionClassificaitonResult(trainDataSet,trainLabel,testDataSet,testLabel):
+    """
+        Description:使用SKL库中的linear_model.LogisticRegression来进行分类预测
+        Params:
+
+        Return:
+
+        Author:
+                HY
+        Modify:
+                2019/5/4 23:24
+    """
+    # LRClassifier=LR(penalty='l2',solver='liblinear',max_iter=50)
+    LRClassifier = LR(penalty='l2', solver='sag', max_iter=2000,C=0.00741)
+    LRClassifier.fit(trainDataSet,trainLabel)
+    result=LRClassifier.score(testDataSet,testLabel)
+    print('SKL库中LogisticRegression算法正确率为：%f %%'%(result*100))
+
 
 
 if __name__ == '__main__':
@@ -171,8 +216,12 @@ if __name__ == '__main__':
     time1=time.time()
     myOwnLogisitcClassificationResult(trainDataSet,trainLabel,testDataSet,testLabel,0)
     time2=time.time()
-    print('Finshed in %d'%(time2-time1))
+    print('Finshed in %f'%(time2-time1))
     time1 = time.time()
     myOwnLogisitcClassificationResult(trainDataSet, trainLabel, testDataSet, testLabel, 1)
     time2 = time.time()
-    print('Finshed in %d' % (time2 - time1))
+    print('Finshed in %f' % (time2 - time1))
+    time1 = time.time()
+    SKLLogisticRegressionClassificaitonResult(trainDataSet,trainLabel,testDataSet,testLabel)
+    time2 = time.time()
+    print('Finshed in %f' % (time2 - time1))
